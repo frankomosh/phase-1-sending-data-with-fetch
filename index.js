5d199a214e05b1cd63d2049ed0df42c098dc79ba
Frank-197 <frankomosh197@gmail.com>
2022-06-16 00:43:00 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Add your code here
+// const details={
+//    method:'POST',
+//    headers:{
+//     'Content-Type':'application/json',
+//     'Accept':'application/json'
+//    },
+//    body: JSON.stringify({
+//        name:'Frank',
+//        email:'frankomosh197@gmail.com'
+//    })
+
+// }
+// fetch('http://localhost:3000/users')
+function submitData(name , email) {
+     //const info = {name, email}
+      return fetch("http://localhost:3000/users",{
+         method:"POST",
+         headers:{
+             "Content-Type": "application/json",
+             "Accept": "application/json",
+         },
+         body: JSON.stringify({
+                   name:'Steve',
+                    email:'steve@steve.com'
+                })
+     })
+     .then(response=>response.json()
+         
+       )
+       .then(data=> {
+           document.body.innerHTML = data.id
+       })
+       .catch( error => {document.body.innerHTML = error.message});
+ } 
