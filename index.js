60d8905750ced0e9060c91855ba02689cff1f810
Frank-197 <frankomosh197@gmail.com>
2022-06-16 08:58:52 +0300

    Done.

--- index.js
+++ index.js
@@ -1,34 +1,25 @@
-// const details={
-//    method:'POST',
-//    headers:{
-//     'Content-Type':'application/json',
-//     'Accept':'application/json'
-//    },
-//    body: JSON.stringify({
-//        name:'Frank',
-//        email:'frankomosh197@gmail.com'
-//    })
-
-// }
-// fetch('http://localhost:3000/users')
-function submitData(name , email) {
-     //const info = {name, email}
-      return fetch("http://localhost:3000/users",{
-         method:"POST",
-         headers:{
-             "Content-Type": "application/json",
-             "Accept": "application/json",
-         },
-         body: JSON.stringify({
-                   name:'Steve',
-                    email:'steve@steve.com'
-                })
-     })
-     .then(response=>response.json()
+ const details={
+     name:'Steve',
+     email:'steve@steve.com'
+ }
+ function submitData() {
+      //const info = {name, email}
+       return fetch("http://localhost:3000/users",{
+          method:"POST",
+          headers:{
+              "Content-Type": "application/json",
+              "Accept": "application/json",
+          },
+          body: JSON.stringify(details)
+      })
+      .then(response=>response.json()
          
-       )
-       .then(data=> {
-           document.body.innerHTML = data.id
-       })
-       .catch( error => {document.body.innerHTML = error.message});
- } 
+        )
+        .then(object=> {
+         return object})
+        
+        //.then(data=> {
+           // document.body.innerHTML = data.id
+        //})
+        .catch( error => {document.body.innerHTML = error.message});
+  } 
